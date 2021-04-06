8d5e1103b034ba7a51c0e471f3c659ee367e56a7
David Steeb <david.steeb@b13.com>
2021-04-06 15:25:52 +0200

    [TASK] Update backend previews for text field, include cropping by default

---
+++ Classes/ViewHelpers/RenderBodytextViewHelper.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace B13\Backendpreviews\ViewHelpers;
+
+/*
+ * This file is part of TYPO3 CMS-extension backendpreviews by b13.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
+
+/**
+ * Class RenderBodytextViewHelper
+ *
+ * View helper to process bodytext field values for backend preview, similar to PageLayoutView:renderText().
+ *
+ * @package B13\Backendpreviews\ViewHelpers
+ */
+class RenderBodytextViewHelper extends AbstractViewHelper
+{
+
+    use CompileWithContentArgumentAndRenderStatic;
+
+    /**
+     * Initialize arguments.
+     *
+     * @return void
+     * @api
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument(
+            'value',
+            'string',
+            'The input value. If not given, the evaluated child nodes will be used.',
+            false,
+            null);
+        $this->registerArgument(
+            'crop',
+            'int',
+            'change the default crop value to this number of characters',
+            '',
+            '1500'
+        );
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param \TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $crop = $arguments['crop'];
+        $value = $arguments['value'];
+        if ($value === null) {
+            $value = $renderChildrenClosure();
+        }
+        if ($value) {
+            $value = strip_tags($value);
+            $value = GeneralUtility::fixed_lgd_cs($value, $crop);
+            return nl2br(htmlspecialchars(trim($value), ENT_QUOTES, 'UTF-8', false));
+        }
+    }
+}
