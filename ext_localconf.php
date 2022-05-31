c45b3cae3696c1dc27000b096ee36b33b920a90e
David Steeb <david.steeb@b13.com>
2022-05-31 18:42:52 +0200

    [TASK] Use non-integer for backend preview hook
    This allows other extensions to add data or change behaviour when using numeric values without regards of install/loading order.

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][1615885039708] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['tx_backendpreviews] =
     \B13\Backendpreviews\Hooks\BackendPreviewRenderer::class;
 
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
